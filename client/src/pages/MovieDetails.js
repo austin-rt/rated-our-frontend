bfb25e1ea34c34c15eb0d01e5eabca03d3f023e5
Alexander Ab <ab.alexande21@gmail.com>
2022-08-15 17:53:09 -0400

    added reviews to movie details page

--- client/src/pages/MovieDetails.js
+++ client/src/pages/MovieDetails.js
@@ -1,19 +1,29 @@
 const MovieDetails = (props) => {
   console.log(props)
   return (
-    <div className="movie-details">
-      <h1 className="movie-title">{props.selectedMovie.title}</h1>
-      <div className="movie-details-image-container">
-        <img
-          className="movie-details-image"
-          src={props.selectedMovie.image}
-          alt={props.selectedMovie.title}
-        />
+    <section className="movie-page">
+      <div className="movie-details">
+        <h1 className="movie-title">{props.selectedMovie.title}</h1>
+        <div className="movie-details-image-container">
+          <img
+            className="movie-details-image"
+            src={props.selectedMovie.image}
+            alt={props.selectedMovie.title}
+          />
+        </div>
+        <div className="movie-description">
+          <p>{props.selectedMovie.body}</p>
+        </div>
       </div>
-      <div className="movie-description">
-        <p>{props.selectedMovie.body}</p>
+      <div className="movie-reviews">
+        {props.selectedMovie.movie_reviews.map((review) => (
+          <div className="review">
+            <h3>Rating: {review.rating}</h3>
+            <h3>{review.body}</h3>
+          </div>
+        ))}
       </div>
-    </div>
+    </section>
   )
 }
 
