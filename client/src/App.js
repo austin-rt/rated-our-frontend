99f5fa53eac8d2078deab91c97ccb078e8819137
Alexander Ab <ab.alexande21@gmail.com>
2022-08-15 15:57:23 -0400

    created api call for movies for home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
-
-import './App.css';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { BASE_URL } from './globals'
 
 function App() {
+  const [movies, setMovies] = useState([])
+
+  //Get All Movies
+  useEffect(() => {
+    const getMovies = async () => {
+      const res = await axios.get(`${BASE_URL}/api/movies`)
+      setMovies(res.data)
+      console.log(res.data)
+    }
+    getMovies()
+  }, [])
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <header></header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home movies={movies} />} />
+        </Routes>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
