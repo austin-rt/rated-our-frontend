76cb4ab5889ce0783f98f1c20dda3d0ddbc32511
Alexander Ab <ab.alexande21@gmail.com>
2022-08-16 17:54:12 -0400

    added logo to nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,14 +13,29 @@ const Nav = ({authenticated, user, handleLogOut }) => {
       )
   }
 
-return (
-  <header>
-    <nav>
+const publicOptions = (
+  <nav>
       <h1>Rated Our</h1>
       <Link to="/" className='link'><button className='nav-button'> Home </button></Link>
       <Link to="/login" className='link'><button className='nav-button'> Login </button></Link>
       <Link to="/about" className='link'><button className='nav-button'> About </button></Link>
-    </nav>
+  </nav>
+)
+
+
+
+return (
+  <header>
+      <Link to="/">
+        <div className="logo-wrapper" alt="logo">
+          <img
+            className="logo"
+            src="https://i.imgur.com/0ceEQPz.png"
+            alt="welcome banner"
+          />
+        </div>
+      </Link>
+      {authenticated && user ? userOptions : publicOptions}
   </header>
 )
 
