1745fb5bae8c508dd7d4ec7c91ecfa864b245cdc
Alexander Ab <ab.alexande21@gmail.com>
2022-08-17 10:43:46 -0400

    added reviewForm route

--- src/App.js
+++ src/App.js
@@ -87,6 +87,7 @@ function App() {
           />
           <Route path="/register" element={<Register />} />
           <Route path="/about" element={<About />} />
+          <Route path="/about" element={<ReviewForm />} />
         </Routes>
       </main>
     </div>
