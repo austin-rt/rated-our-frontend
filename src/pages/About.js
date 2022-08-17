dd49030d7b54c2bd35db5d66512ef0d1033bce51
Deonte LaBorde <deonte.laborde@gmail.com>
2022-08-17 11:28:44 -0400

    created About Us boilerplate

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,25 @@
-const About = () => {}
+const About = () => {
+  return (
+    <div class="about-section">
+      <h1> About Us</h1>
 
+      <div class='project details'>
+      <h2>The Project</h2>
+      <p> Insert details about the project, the technology, and how it works</p>
+      </div>
+
+      <div class="team">
+        <h2>Meet the Team</h2>
+        <h4>Person 1</h4>
+        <p> Details about person</p>
+        <h4>Person 2</h4>
+        <p> Details about person</p>
+        <h4>Person 3</h4>
+        <p> Details about person</p>
+        <h4>Person 4</h4>
+        <p> Details about person</p>
+      </div>
+    </div>
+  )
+}
 export default About
