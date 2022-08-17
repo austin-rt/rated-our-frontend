d204268094c87d463b8883a690ddc365083efe09
austin-rt <austintaylorart@gmail.com>
2022-08-17 19:16:48 -0400

    add review delete button

--- src/services/Authorize.js
+++ src/services/Authorize.js
@@ -1,38 +1,48 @@
-import Client from './api'
+import Client from './api';
 
 export const RegisterUser = async (data) => {
   try {
-    const res = await Client.post('/users/register', data)
-    return res.data
+    const res = await Client.post('/users/register', data);
+    return res.data;
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 export const CreateReview = async (data) => {
   try {
-    const res = await Client.post('/reviews/:movie_id/:user_id', data)
-    return res.data
+    const res = await Client.post('/reviews/:movie_id/:user_id', data);
+    return res.data;
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
+
+export const DestroyReview = async (data) => {
+  try {
+    const res = await Client.delete(`/reviews/${data}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
 
 export const LoginUser = async (data) => {
   try {
-    const res = await Client.post('/users/login', data)
-    localStorage.setItem('token', res.data.token)
-    return res.data.user
+    const res = await Client.post('/users/login', data);
+    localStorage.setItem('token', res.data.token);
+    return res.data.user;
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 export const CheckSession = async () => {
   try {
-    const res = await Client.get('/users/session')
-    return res.data
+    const res = await Client.get('/users/session');
+    return res.data;
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
