c53d4d8c2fb96ec0a2fac229d55d75da487b39fa
Deonte LaBorde <deonte.laborde@gmail.com>
2022-08-18 14:33:26 -0400

    remove title text from movie screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,6 @@ const Home = (props) => {
       <div className="movies-grid">
         {props.movies.map((movie) => (
           <div className="movie-card" key={movie.id}>
-            <h2 className="main-title">{movie.title}</h2>
             <div onClick={() => props.chooseMovie(movie)}>
               <img src={movie.image} alt={movie.title} />
             </div>
