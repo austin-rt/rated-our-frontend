fa351ac1d0368b52798415a7f7f57a15684d067e
austin-rt <austinrtaylor91@gmail.com>
2022-11-30 14:57:17 -0500

    correct base url

--- src/globals.js
+++ src/globals.js
@@ -1,9 +1,1 @@
-const BASE_URL = 'https://rated-our-api.up.railway.app/api';
-<<<<<<< HEAD
-// const BASE_URL = 'http://localhost:3001/api'
-=======
-// const BASE_URL = 'http://localhost:3000/api'
->>>>>>> 9a1fd42b648e4495decf529785bf73fc61a6b1ce
-module.exports = {
-  BASE_URL,
-};
+export const BASE_URL = 'https://rated-our-api.up.railway.app/api';
